8d12c4f4a4966fcc2e144ae61d9b3a26aafd4308
Marius Obert <obatzer@gmail.com>
2023-01-16 17:22:53 +0100

    shorten code

--- send-alpha.js
+++ send-alpha.js
@@ -1,14 +1,11 @@
-const dotenv = require("dotenv");
-const twilio = require("twilio");
+require("dotenv").config();
+const client = require("twilio")();
 
-dotenv.config();
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
-//require("dotenv").config();
-//const client = require("twilio")();
-
-client.messages.create({
-    body: "This message comes from an alphanumeric sender ID!",
-    to: "+49111111111",
-    from: "MG000000000000"
-}).then((message) => console.log(message.sid))
+client.messages
+    .create({
+        body: "This message comes from an alphanumeric sender ID!",
+        to: "+123456789", //TODO enter your phone number here
+        from: "MG00000000" //TODO enter the SID of the Messaging service you saved earlier
+    })
+    .then(console.log)
+    .catch(console.error)
